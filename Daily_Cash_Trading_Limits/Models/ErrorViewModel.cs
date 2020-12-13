8636f39608de9daadf5ac6c333144007941afa5a
Justin <jgjudd40@gmail.com>
2020-12-13 18:22:36 -0500

    initial project scaffold
    just finished marathon installing a lot of things, so hopefully everything is where it needs to be

---
+++ Daily_Cash_Trading_Limits/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Daily_Cash_Trading_Limits.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
